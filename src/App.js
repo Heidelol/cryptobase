e2a12360e339655fc993b368d425f4035b9d1d0d
Heidel <heideloliver@gmail.com>
2022-06-16 08:27:50 -0500

    proyecto nuevo

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useEffect,useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import {ThemeProvider} from './context/ThemeContext'
+import Home from './routes/Home'
+import Navbar from './components/Navbar'
+import SignIn from './routes/SignIn'
+import SignUp from './routes/SignUp'
+import Account from './routes/Account'
+import CoinPage from './routes/CoinPage'
+import axios from 'axios';
+
+
+function App() {
+
+  const [coins, setCoins] = useState([])
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true'
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data)
+     
+    })
+  },[url])
+  
+  return (
+    <ThemeProvider>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home coins={coins} />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/account' element={<Account />} />
+        <Route path='/coin/:coinId' element={<CoinPage />}>
+          <Route path=':coinId' />
+        </Route>
+      </Routes>
+    </ThemeProvider>
+   
+  );
+}
+
+export default App;
---
+++ src/routes/CoinPage.jsx
@@ -0,0 +1,22 @@
+import React,{useEffect, useState} from 'react'
+import axios from 'axios'
+
+function CoinPage() {
+
+  const [coin, setCoin] = useState({})
+
+  const url = 'https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&sparkline=true'
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoin(response.data)
+      console.log(response.data)
+    })
+  },[url])
+
+  return (
+    <div>CoinPage</div>
+  )
+}
+
+export default CoinPage
